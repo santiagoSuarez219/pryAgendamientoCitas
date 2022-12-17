2a73c789764a43a047f934212ae18dff99a8c033
KAMBLACK66 <93850511+KAMBLACK66@users.noreply.github.com>
2022-12-17 09:27:22 -0500

    despliegue

--- FrontEnd/pryagendamientocitas/src/components/Config/ApiUrl.js
+++ FrontEnd/pryagendamientocitas/src/components/Config/ApiUrl.js
@@ -1,2 +1,2 @@
-export const API = path=>`http://localhost:8080/api/usuario/${path}`
-export const APIC = path=>`http://localhost:8080/api/cita/${path}`
+export const API = path=>`http://150.136.176.8:8080/agendamientocitas/api/usuario/${path}`
+export const APIC = path=>`http://150.136.176.8:8080/agendamientocitas/api/cita/${path}`
